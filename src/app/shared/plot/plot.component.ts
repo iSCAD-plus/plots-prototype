80d74692e59a3d8501fd64a96c19e0f1ecca9a16
Koleok <kchamb3@gmail.com>
2017-05-15 10:47:37 -0400

    Shuffle subscribe function a little

--- src/app/shared/plot/plot.component.ts
+++ src/app/shared/plot/plot.component.ts
@@ -6,6 +6,8 @@ import { Apollo } from 'apollo-angular';
 import * as pieChart from '../shapers/pie-chart';
 import * as discreteBarChart from '../shapers/discrete-bar-chart';
 
+const chartDataPoint = (x, y) => p => ({ value: p[x], label: p[y] });
+
 const presentData = S.pipe([
   R.map(R.omit(['__typename'])),
   x => JSON.stringify(x, null, 2),
@@ -27,7 +29,6 @@ interface Axis {
   template: `
   <h3>{{title}}</h3>
   <form>
-      {{query}}
       <input
         type="radio"
         name="chartType"
@@ -80,33 +81,24 @@ export class PlotComponent implements OnInit {
   ngOnInit() {
     const { x, y } = this.axisInfo;
     const plotType = this.plotType || 'discreteBarChart';
-    this.shapers = {
-      pieChart,
-      discreteBarChart,
-    };
+    this.shapers = { pieChart, discreteBarChart };
     this.shaper = R.prop(plotType)(this.shapers);
-
     this.options = this.shaper.options({ x, y });
 
-    const subscriber = ({ data }) => {
-      this.loading = data.loading;
-
-      const values = R.map(S.pipe([
-        R.props([x, y]),
-        R.zipObj(['value', 'label']),
-      ]), data.decisionQuery);
-
-      this.preppedData = {
-        key: data.decisionQuery[0]['seriesKey'] || this.title,
-        values,
-      };
-
-      this.rawData = presentData(data.decisionQuery);
-      this.chartData = this.shaper.shape(this.preppedData);
-    };
-
     this.apollo.query<QueryResponse>({ query: this.gqlQuery })
-      .subscribe(subscriber);
+      .subscribe(({ data }) => {
+        this.loading = data.loading;
+
+        const values = data.decisionQuery.map(chartDataPoint(x, y));
+
+        this.preppedData = {
+          key: data.decisionQuery[0]['seriesKey'] || this.title,
+          values,
+        };
+
+        this.rawData = presentData(data.decisionQuery);
+        this.chartData = this.shaper.shape(this.preppedData);
+      });
   }
 
   public setPlotType(plotType: string) {
@@ -116,9 +108,5 @@ export class PlotComponent implements OnInit {
     this.shaper = R.prop(this.plotType)(this.shapers);
     this.options = this.shaper.options({ x, y });
     this.chartData = this.shaper.shape(this.preppedData);
-
-    //console.log(this.title);
-    //console.log(this.preppedData);
-    //console.log(this.chartData);
   }
 }
